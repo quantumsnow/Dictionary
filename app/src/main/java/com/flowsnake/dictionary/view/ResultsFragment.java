3354a9ccdc13f14f2464c7510706d3e16966ea5f
Jakob Scharmberg <jakobtheai@gmail.com>
2016-06-10 20:57:11 +0200

    Fixed: project setup and runability

--- app/src/main/java/com/flowsnake/dictionary/view/ResultsFragment.java
+++ app/src/main/java/com/flowsnake/dictionary/view/ResultsFragment.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.flowsnake.dictionary.R;
 
@@ -58,7 +57,7 @@ public class ResultsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_results, container, false);
 
         // TODO: get result
-        ((TextView) rootView.findViewById(R.id.textView)).setText(term);
+        //((TextView) rootView.findViewById(R.id.textView)).setText(term);
 
         return rootView;
     }
